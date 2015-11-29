9d144c47ad6967dfde46316c6928c3405a7f9b88
Yannic Heckmann <heckinator97@gmx.de>
2015-11-29 18:38:22 +0100

    .gitignore is now working

--- .old/mobile/res/php/_index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include(dirname(__FILE__)."/../../../res/php/_auth.php");
-	include(dirname(__FILE__)."/_getWeekdays.php");
-	include(dirname(__FILE__)."/../../../res/php/_checkDataBase.php");
-	include(dirname(__FILE__)."/../../../res/php/_loadLangFiles.php");
-	include(dirname(__FILE__)."/../../../res/php/_getVersionScript.php");
-?>
--- .old/res/php/_getVersionScript.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$versionFile = @fopen("https://dl.dropboxusercontent.com/u/107727443/stundenplanVersion.txt", "r");
-		
-	if ($versionFile != null)
-		$version = fgets($versionFile);
-	else
-		$version = "???";	
-?>
