ca25d9df43e777a5f32482c9bab5cac6875fb2bb
Yannic Heckmann <heckinator97@gmx.de>
2015-12-07 23:28:38 +0100

    - The .ini file now works correctly - Cleaned up code - Added two static functions to the FileLoader class 'Create_new_file' and 'Create_new_folder' in Utilities.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 	define('PROJECT_DIR', dirname(__FILE__));
+	define('HTTP_HOST', $_SERVER['HTTP_HOST']);
 
 	require_once(PROJECT_DIR.'/resources/library/php/Utilities.php');
+
 	//Register
 	FileLoader::Register('Resources.Config', PROJECT_DIR.'/resources/config');
 	FileLoader::Register('Resources.Data', PROJECT_DIR.'/resources/data');
@@ -11,32 +13,12 @@
 	FileLoader::Register('Resources.Php', PROJECT_DIR.'/resources/php');
 
 	// Parse .ini Files
-	if (file_exists(PROJECT_DIR.'/resources/config/'.$_SERVER['HTTP_HOST'].'.config.ini'))
-		IniParser::Parse(PROJECT_DIR.'/resources/config/'.$_SERVER['HTTP_HOST'].'.config.ini', false);
-	else
-	{
-		$array = explode('/', PROJECT_DIR.'/resources/config/'.$_SERVER['HTTP_HOST'].'.config.ini');
-		$filepath = '';
-		
-		for ($i = 0; $i < count($array); $i++) {
-			$filepath = $filepath.$array[$i].'/';
-			
-			if (!file_exists($filepath)){
-				$fp = fopen(PROJECT_DIR.'/resources/config/'.$_SERVER['HTTP_HOST'].'.config.ini', 'w');
-				fwrite($fp, file_get_contents(PROJECT_DIR.'/resources/config/default.config.ini'));
-				fclose($fp);
-			}
-		}
-		IniParser::Parse(PROJECT_DIR.'/resources/config/'.$_SERVER['HTTP_HOST'].'.config.ini', false);
-	}
+	if (!file_exists(PROJECT_DIR.'/resources/config/'.HTTP_HOST.'.config.ini'))
+		FileLoader::Create_new_file(PROJECT_DIR.'/resources/config/'.HTTP_HOST.'.config.ini', file_get_contents(PROJECT_DIR.'/resources/config/default.config.ini'));
+	IniParser::Parse(PROJECT_DIR.'/resources/config/'.HTTP_HOST.'.config.ini', false);
 
 	FileLoader::Load('Resources.Library.Php.BingImageHandler');
   	FileLoader::Load('Resources.Library.Php.LanguageHandler');
-
-  	// FileLoader::Load('Resources.Library.Php.LogHandler');
-  	// LogHandler::Log(basename(__FILE__, '.php'), 'Test', 'ERROR');
-
-  	var_dump($_SERVER['HTTP_HOST']);
 
   	if (!defined('LANG')) LanguageHandler::ParseLangFile();
 
